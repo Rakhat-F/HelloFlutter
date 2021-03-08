3b1df2bc75d3e166686cb30eed242051892ae0c0
Rakhat-F <63895123+Rakhat-F@users.noreply.github.com>
2021-03-08 19:10:24 +0600

    first commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       scaffoldBackgroundColor: Colors.black,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, Flutter',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50 ,
+                  color: Colors.red.withOpacity(0.8)),
+            ),
+          ],
+        ),
+      ),
+      );
+  }
+}
